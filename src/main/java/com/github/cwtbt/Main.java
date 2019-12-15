d72674793b3afacc47341a7ec5ac46f3cc840c57
CWTBT <lw03232@gmail.com>
2019-12-14 23:37:30 -0600

    Added pingpong bot framework

--- src/main/java/com/github/cwtbt/Main.java
+++ src/main/java/com/github/cwtbt/Main.java
@@ -1,7 +1,18 @@
 package com.github.cwtbt;
 
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        String token = "NjU1NjQxMTA1NzY0OTc0NjA1.XfXEpg.TefAYylAfpwKBJSev1b2Yj77y4k";
+
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("!ping")) {
+                event.getChannel().sendMessage("Pong!");
+            }
+        });
     }
 }
